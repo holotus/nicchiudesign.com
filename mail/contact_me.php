df1ab64e05714f6a18d6b355c64992df278bb834
Nicholas Chiu <nicchiuonline@gmail.com>
2015-07-29 12:12:28 -0400

    Email update 2015.07.29

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -24,3 +24,34 @@ $headers .= "Reply-To: $email_address";
 mail($to,$email_subject,$email_body,$headers);
 return true;			
 ?>
+
+
+
+
+$.ajax({
+  type: “POST”,
+  url: “https://mandrillapp.com/api/1.0/messages/send.json”,
+  data: {
+    ‘key’: ‘gapuPWYuZAaq2zo1zv_fxw’,
+    ‘message’: {
+      ‘from_email’: ‘nicchiudesign@gmail.com’,
+      ‘to’: [
+          {
+            ‘email’: ‘RECIPIENT_NO_1@EMAIL.HERE’,
+            ‘name’: ‘RECIPIENT NAME (OPTIONAL)’,
+            ‘type’: ‘to’
+          },
+          {
+            ‘email’: ‘RECIPIENT_NO_2@EMAIL.HERE’,
+            ‘name’: ‘ANOTHER RECIPIENT NAME (OPTIONAL)’,
+            ‘type’: ‘to’
+          }
+        ],
+      ‘autotext’: ‘true’,
+      ‘subject’: ‘YOUR SUBJECT HERE!’,
+      ‘html’: ‘YOUR EMAIL CONTENT HERE! YOU CAN USE HTML!’
+    }
+  }
+ }).done(function(response) {
+   console.log(response); // if you're into that sorta thing
+ });
